aab44e62de1812204c3b9f8fb1222e6a2f8d61cd
Celyne <dianakydd2@gmail.com>
2024-03-20 11:48:24 +0300

    updated api.dart

--- dart_application_1/bin/api.dart
+++ dart_application_1/bin/api.dart
@@ -1,8 +1,24 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() async {
+  var data = await fetchAlbum();
+
+  for (var i = 0; i < 100; i++) {
+    print(data[i]['userId']);
+    print(data[i]['id']);
+    print(data[i]['title']);
+    print('------------------');
+  }
+}
+
+Future<dynamic> fetchAlbum() async {
   final url = 'https://jsonplaceholder.typicode.com/albums';
   var res = await http.get(Uri.parse(url));
-
-  print(res.body);
+  if (res.statusCode == 200) {
+    var obj = json.decode(res.body);
+    return obj;
+  } else {
+    print('Error');
+  }
 }
