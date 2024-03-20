2f99765879a0660a4ece999d6378c392ea6fa255
Celyne <dianakydd2@gmail.com>
2024-03-20 11:24:14 +0300

    Added res

--- dart_application_1/bin/api.dart
+++ dart_application_1/bin/api.dart
@@ -2,5 +2,7 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   final url = 'https://jsonplaceholder.typicode.com/albums';
-  await http.get(Uri.parse(url));
+  var res = await http.get(Uri.parse(url));
+
+  print(res.body);
 }
