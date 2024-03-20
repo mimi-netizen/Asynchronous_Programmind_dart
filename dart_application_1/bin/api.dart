e5184b9ae1d9d0075b8d3ac1088b1eb68cb8659c
Celyne <dianakydd2@gmail.com>
2024-03-20 11:21:09 +0300

    intergrates async await

--- dart_application_1/bin/api.dart
+++ dart_application_1/bin/api.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
 
-void main() {
+void main() async {
   final url = 'https://jsonplaceholder.typicode.com/albums';
-  http.get(Uri.parse(url));
+  await http.get(Uri.parse(url));
 }
